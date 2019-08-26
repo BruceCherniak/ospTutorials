9a4c5a65e3d34dc9e8bb7f3268a9faf27ff9a219
Bruce Cherniak <bruce.cherniak@intel.com>
2019-08-26 14:52:00 -0500

    Made winbuild.bat check for ospray_DIR setting

--- winbuild.bat
+++ winbuild.bat
@@ -1,9 +1,15 @@
 echo off
 rem Windows Build Steps
 rem Note: Might need to change "Visual Studio..." depending on user's install
-cmake -G "Visual Studio 15 2017 Win64" ..  
-cmake --build . --config Release
+rem Create and cd into a build directory and then run this file with ..\winbuild.bat
 
+if not exist "%ospray_DIR%" (
+  echo "Must 'set ospray_DIR=<OSPRay Binary Package>'"
+) else (
+  cmake -G "Visual Studio 15 2017 Win64" ..  
+  cmake --build . --config Release
 
-rem To run, we need *.dll from the OSPRay binary install
-rem copy <path to OSPRay-1.8.5.windows>\bin\*.dll Release
+  rem To run, we need *.dll from the OSPRay binary install
+  copy %ospray_DIR%\bin\*.dll Release\
+)
+
